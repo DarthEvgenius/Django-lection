ed3ebd87d586cc57ab3ed7a5179a75cc6ee46ae9
“Eugene_Zinin” <EugeneZinin@gmail.com>
2023-02-09 16:51:57 +0300

    Add /flight page

--- lect4-Dj_migrations/airline/flights/views.py
+++ lect4-Dj_migrations/airline/flights/views.py
@@ -4,6 +4,19 @@ from .models import Flight
 
 # Create your views here.
 def index(request):
+	""" Shows all the flihts """
+
 	return render(request, "flights/index.html", {
 		"flights": Flight.objects.all()
+	})
+
+def flight(request, flight_id):
+	""" Shows info about a flight """
+
+	# Get the flight, "pk" here is for "Primary Key" of the table, for id precisely
+	flight = Flight.objects.get(pk=flight_id)
+
+	# Render a template with this flight
+	return render(request, "flights/flight.html", {
+		"flight": flight
 	})
