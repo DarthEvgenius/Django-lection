7a2f9e722816c0062d43e062b8cef5ed894043cf
“Eugene_Zinin” <EugeneZinin@gmail.com>
2023-02-09 17:58:18 +0300

    Add /book path

--- lect4-Dj_migrations/airline/flights/views.py
+++ lect4-Dj_migrations/airline/flights/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
-from .models import Flight
+from .models import Flight, Passenger
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+
 def index(request):
 	""" Shows all the flihts """
 
@@ -21,5 +25,25 @@ def flight(request, flight_id):
 	return render(request, "flights/flight.html", {
 		"flight": flight,
 		# A "flight" doesn't have a "passengers" list inside, but due to "related_name" in models "Passenger" we can get access to this list
-		"passengers": flight.passengers.all()
+		"passengers": flight.passengers.all(),
+		# We exclude the people that are not passengers of this flight, passengers that don't have this flight in their list of flights
+		"non_passengers": Passenger.objects.exclude(flights=flight).all()
 	})
+
+
+def book(request, flight_id):
+	""" We can book flights for passengers """
+	# Needs an error check for invalid flights or passengers
+
+	# Via "GET" method ==> view the page, via "POST" ==> update database
+	if request.method == "POST":
+		# Get the flight
+		flight = Flight.objects.get(pk=flight_id)
+		# Get the passenger from the input name="passenger"
+		passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+		# Add this flight to the passenger's list
+		passenger.flights.add(flight)
+		# Render a page, redirect
+		# reverse takes a name of some view and finds out the url
+		# Pay attention to args syntax: it's a tuple, so we have <,> at the end inside ()
+		return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
